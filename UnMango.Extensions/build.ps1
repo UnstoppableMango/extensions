e606a078f2f99ca21abdbfefa86e4b43337fadda
Erik Rasmussen <ecr.referee@hotmail.com>
2019-06-18 23:23:43 -0500

    Copy files from other project

---
+++ UnMango.Extensions/build.ps1
@@ -0,0 +1,60 @@
+#!/usr/bin/env pwsh
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [ValidateSet('Debug', 'Release')]
+    $Configuration = $null,
+    [switch]
+    $ci,
+    [switch]
+    $sign,
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$MSBuildArgs
+)
+
+Set-StrictMode -Version 1
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Force -Scope Local "$PSScriptRoot/src/common.psm1"
+
+#
+# Main
+#
+
+if ($env:CI -eq 'true') {
+    $ci = $true
+}
+
+if (!$Configuration) {
+    $Configuration = if ($ci) { 'Release' } else { 'Debug' }
+}
+
+if ($ci) {
+    $MSBuildArgs += '-p:CI=true'
+}
+
+$isPr = ($env:BUILD_REASON -eq 'PullRequest')
+if (-not (Test-Path variable:\IsCoreCLR)) {
+    $IsWindows = $true
+}
+
+$artifacts = "$PSScriptRoot/artifacts/"
+
+Remove-Item -Recurse $artifacts -ErrorAction Ignore
+exec dotnet msbuild /t:UpdateCiSettings @MSBuildArgs
+exec dotnet build --configuration $Configuration @MSBuildArgs
+exec dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts @MSBuildArgs
+
+[string[]] $testArgs=@()
+if ($PSVersionTable.PSEdition -eq 'Core' -and -not $IsWindows) {
+    $testArgs += '--framework','netcoreapp2.2'
+}
+if ($env:TF_BUILD) {
+    $testArgs += '--logger', 'trx'
+}
+
+#exec dotnet test --no-restore --no-build --configuration $Configuration '-clp:Summary' `
+#    "$PSScriptRoot/test/KG.Loyalty.MobGate.Tests/KG.Loyalty.MobGate.Tests.csproj" `
+#    @testArgs `
+#    @MSBuildArgs
+
+write-host -f magenta 'Done'
