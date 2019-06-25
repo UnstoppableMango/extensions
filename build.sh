af01c532fadd21cbfe5155cae207aaf45c4a5773
Erik Rasmussen <ecr.referee@hotmail.com>
2019-06-24 20:25:56 -0500

    More robust input args
    Taken from microsoft script of course

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
   [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
@@ -16,23 +16,73 @@ Architecture=
 Configuration=
 
 while [[ $# > 0 ]]; do
-  lowerI="$(echo $1 | awk '{print tolower($0)}')"
-  case $lowerI in
-    -a|--architecture)
-      Architecture="$2"
-      args+=("/p:Architecture=$Architecture")
-      shift
-	    ;;
-    -c|--configuration)
-      Configuration="$2"
+  opt="$(echo "${1/#--/-}" | awk '{print tolower($0)}')"
+  case "$opt" in
+    -help|-h)
+      usage
+      exit 0
+      ;;
+    -configuration|-c)
+      configuration=$2
       shift
       ;;
-    --ci)
+    -verbosity|-v)
+      verbosity=$2
+      shift
+      ;;
+    -binarylog|-bl)
+      binary_log=true
+      ;;
+    -pipelineslog|-pl)
+      pipelines_log=true
+      ;;
+    -restore|-r)
+      restore=true
+      ;;
+    -build|-b)
+      build=true
+      ;;
+    -rebuild)
+      rebuild=true
+      ;;
+    -pack)
+      pack=true
+      ;;
+    -test|-t)
+      test=true
+      ;;
+    -integrationtest)
+      integration_test=true
+      ;;
+    -performancetest)
+      performance_test=true
+      ;;
+    -sign)
+      sign=true
+      ;;
+    -publish)
+      publish=true
+      ;;
+    -preparemachine)
+      prepare_machine=true
+      ;;
+    -projects)
+      projects=$2
+      shift
+      ;;
+    -ci)
       ci=true
+      ;;
+    -warnaserror)
+      warn_as_error=$2
+      shift
+      ;;
+    -nodereuse)
+      node_reuse=$2
       shift
       ;;
     *)
-      args+=("$1")
+      properties="$properties $1"
       ;;
   esac
 
@@ -51,19 +101,19 @@ fi
 
 if [[ -z "$Configuration" ]]; then
   if [[ "$ci" == true ]]; then
-    $Configuration="Release";
+    Configuration="Release";
   else
-    $Configuration="Debug";
+    Configuration="Debug";
   fi
 fi
 
-[ "$BUILD_REASON" == "PullRequest" ] || isPr=true
+[[ "$BUILD_REASON" == "PullRequest" ]] && isPr=true
 
 artifacts="$REPOROOT/artifacts/"
 
-rm -rf $artifacts
+rm -rf ${artifacts}
 execute dotnet msbuild /t:UpdateCiSettings "${args[@]}"
-execute dotnet build --configuration $Configuration "${args[@]}"
-execute dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts "${args[@]}"
+execute dotnet build --configuration ${Configuration} "${args[@]}"
+execute dotnet pack --no-restore --no-build --configuration ${Configuration} -o ${artifacts} "${args[@]}"
 
 echo "\e[35mDone"
