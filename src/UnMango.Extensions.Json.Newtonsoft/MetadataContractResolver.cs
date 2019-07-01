0ff21b160fce6fff5b252b67794e3feb5dc4cdf0
Erik Rasmussen <ecr.referee@hotmail.com>
2019-06-30 20:32:52 -0500

    Flesh out contract resolver a little more

--- src/UnMango.Extensions.Json.Newtonsoft/MetadataContractResolver.cs
+++ src/UnMango.Extensions.Json.Newtonsoft/MetadataContractResolver.cs
@@ -1,20 +1,43 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
 using UnMango.Extensions.Json.Metadata;
 
 namespace UnMango.Extensions.Json.Newtonsoft
 {
     public class MetadataContractResolver : DefaultContractResolver
     {
-        private readonly JsonMetadata _options;
+        private readonly IDictionary<Type, JsonMetadata> _metadata;
+        private Type _objectType;
 
-        public MetadataContractResolver(JsonMetadata options)
+        public MetadataContractResolver(IEnumerable<JsonMetadata> metadata)
         {
-            _options = options;
+            _metadata = metadata.ToDictionary(x => x.Type, x => x);
+        }
+
+        public string GetPropertyName(string propertyName)
+        {
+            if (_objectType == null) return null;
+
+            if (_metadata.TryGetValue(_objectType, out var metadata))
+            {
+                return metadata.GetPropertyName(propertyName);
+            }
+
+            return null;
+        }
+
+        protected override JsonContract CreateContract(Type objectType)
+        {
+            _objectType = objectType;
+
+            return base.CreateContract(objectType);
         }
 
         protected override string ResolvePropertyName(string propertyName)
         {
-            return _options.GetPropertyName(propertyName) ?? base.ResolvePropertyName(propertyName);
+            return GetPropertyName(propertyName) ?? base.ResolvePropertyName(propertyName);
         }
     }
 }
