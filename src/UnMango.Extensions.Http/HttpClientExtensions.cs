84cdb44464d20f056ff014adb326fa0255fdb596
Erik Rasmussen <ecr.referee@hotmail.com>
2019-06-30 21:08:21 -0500

    Add HTTP extensions project and extension methods

---
+++ src/UnMango.Extensions.Http/HttpClientExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnMango.Extensions.Http
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
+            this HttpClient client,
+            string requestUri,
+            T value,
+            JsonSerializerOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+
+            return SendAsJsonAsync(client, request, value, options, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
+            this HttpClient client,
+            Uri requestUri,
+            T value,
+            JsonSerializerOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+
+            return SendAsJsonAsync(client, request, value, options, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
+            this HttpClient client,
+            string requestUri,
+            T value,
+            JsonSerializerOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Put, requestUri);
+
+            return SendAsJsonAsync(client, request, value, options, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
+            this HttpClient client,
+            Uri requestUri,
+            T value,
+            JsonSerializerOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Put, requestUri);
+
+            return SendAsJsonAsync(client, request, value, options, cancellationToken);
+        }
+
+        public static async Task<HttpResponseMessage> SendAsJsonAsync<T>(
+            this HttpClient client,
+            HttpRequestMessage request,
+            T value,
+            JsonSerializerOptions serializerOptions,
+            CancellationToken cancellationToken = default)
+        {
+            using var stream = new MemoryStream();
+            request.Content = new StreamContent(stream);
+
+            await JsonSerializer.WriteAsync(value, stream, serializerOptions, cancellationToken).ConfigureAwait(false);
+
+            return await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/UnMango.Extensions.Http/HttpContentExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnMango.Extensions.Http
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<T> ReadAsJsonAsync<T>(
+            this HttpContent content,
+            JsonSerializerOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            using var stream = await content.ReadAsStreamAsync().ConfigureAwait(false);
+
+            return await JsonSerializer.ReadAsync<T>(stream, options, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
