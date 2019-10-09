e149e23e6af0684988e174c03954bbdb2dcfdb3c
Erik Rasmussen <ecr.referee@hotmail.com>
2019-10-08 22:43:31 -0500

    WIP on DI extension methods

--- src/Repository/Repository.EntityFrameworkCore/ServiceCollectionExtensions.cs
+++ src/Repository/Repository.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using UnMango.Extensions.Repository;
 
-namespace UnMango.Extensions.Repository
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        public const ServiceLifetime DEFAULT_LIFETIME = ServiceLifetime.Scoped;
+
+        public static IServiceCollection AddRepository<TService, TImplementation>(
+            this IServiceCollection services,
+            ServiceLifetime lifetime = DEFAULT_LIFETIME)
+            where TService : IRepository
+            where TImplementation : TService
+        {
+            var descriptor = ServiceDescriptor.Describe(typeof(TService), typeof(TImplementation), lifetime);
+
+            services.Add(descriptor);
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepository<TRepository, TEntity>(
+            this IServiceCollection services,
+            TRepository repository)
+            where TRepository : IRepository<TEntity>
+        {
+            var descriptor = ServiceDescriptor.Singleton<IRepository<TEntity>>(repository);
+
+            services.Add(descriptor);
+
+            return services;
+        }
     }
 }
